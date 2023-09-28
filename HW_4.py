67582bcaa88de35518585bdfb9a972fcdfe3117c
Anna <aechigrina@gmail.com>
2023-09-29 00:39:36 +0300

    modified:   HW_4.py

--- HW_4.py
+++ HW_4.py
@@ -4,13 +4,13 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества. 
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 
-from random import randint
-n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
-print(n_set)
-m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
-print(m_set)
-s_set = sorted(n_set.intersection(m_set))
-print(*s_set)
+# from random import randint
+# n_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов первого множества: '))))
+# print(n_set)
+# m_set = set(randint(1, 20) for i in range(int(input('Введите кол-во элементов второго множества: '))))
+# print(m_set)
+# s_set = sorted(n_set.intersection(m_set))
+# print(*s_set)
 
 # Задача 24: В фермерском хозяйстве в Карелии выращивают чернику. 
 # Она растёт на круглой грядке, причём кусты высажены только по окружности. 
@@ -23,3 +23,16 @@ print(*s_set)
 # собирает ягоды с этого куста и с двух соседних с ним.
 # Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль, 
 # находясь перед некоторым кустом заданной во входном файле грядки.
+
+from random import randint
+list_1 = list(randint(1, 5) for i in range(int(input('Введите кол-во кустов: '))))
+print(list_1)
+a = int(input('Введите № куста: '))
+res = 0
+if a == 1:
+    res = list_1[0] + list_1[1] + list_1[-1]
+elif a == len(list_1):
+    res = list_1[-2] + list_1[-1] + list_1[0]
+else:
+    res = list_1[a-1] + list_1[a-2] + list_1[a]
+print(res, 'ягод')
